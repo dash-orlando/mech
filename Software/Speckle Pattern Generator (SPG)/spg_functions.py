62c2e37d5925d443541c61337ea6ad7c7f1866eb
Fluvio Lobo <fluvio.lobo@gmail.com>
2017-03-17 14:17:37 -0400

    Began creating algorithms that will be used to automatically generate drawings and 3d shapes.

---
+++ Software/Speckle Pattern Generator (SPG)/spg_functions.py
@@ -0,0 +1,7 @@
+"""
+    Speckle Pattern Generator (SPG)
+    -- Functions Module
+
+    Fluvio L Lobo Fenoglietto 03/17/2017
+"""
+
---
+++ Software/Speckle Pattern Generator (SPG)/spg_test_script.py
@@ -0,0 +1,6 @@
+"""
+    Speckle Pattern Generator (SPG)
+    -- Test Script
+
+    Fluvio L Lobo Fenoglietto 03/17/2017
+"""
