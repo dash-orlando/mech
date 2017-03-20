8beea39c976c7cdca7a89b8cd6a66a9bb605e953
Fluvio Lobo <fluvio.lobo@gmail.com>
2017-03-19 21:48:11 -0400

    Starting testing some of the python dxf commands to create a drawings that can be imported into OnShape.

--- Software/Speckle Pattern Generator (SPG)/spg_test_script.py
+++ Software/Speckle Pattern Generator (SPG)/spg_test_script.py
@@ -7,5 +7,17 @@
 
 try:
     from    dxfwrite    import  DXFEngine   as  dxf
+
+    status = 0
+    
 except ImportError as iErr:
     print iErr
+    print "Download the dxfwrite module using pip"
+
+    status = -1
+
+if status == 0:
+
+    drawing = dxf.drawing('test.dxf')
+    drawing.add(dxf.arc(radius=20.0, center=(2.0, 2.0)))
+    drawing.save()
